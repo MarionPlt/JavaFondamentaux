3b7926395f9c49fb3466f1b0c42212e623f26b9a
MarionJS <74191216+MarionJS@users.noreply.github.com>
2020-11-24 12:06:25 +0100

    Add files via upload

---
+++ JavaFondamentauxTP/src/Client.java
@@ -0,0 +1,14 @@
+public class Client {
+
+    String nom;
+    String prenom;
+    String mail;
+    int filmLoue;
+
+    Client(String nom, String prenom, String mail){
+        this.nom=nom;
+        this.prenom=prenom;
+        this.mail=mail;
+    }
+
+}
